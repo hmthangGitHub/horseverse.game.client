c2a385506de92319b326711a7ef1da465233e9b0
thang.hoang <bbsort123@gmail.com>
2022-06-02 01:21:58 +0700

    update ui quick race

--- UnityProject/HorseVerse/Assets/App/Scripts/UI/UIComponentImplementation/UIQuickMode/UIQuickMode.cs
+++ UnityProject/HorseVerse/Assets/App/Scripts/UI/UIComponentImplementation/UIQuickMode/UIQuickMode.cs
@@ -8,28 +8,34 @@ public class UIQuickMode : PopupEntity<UIQuickMode.Entity>
     public class Entity
     {
         public ButtonComponent.Entity backBtn;
-        public FormattedTextComponent.Entity energy;
         public ButtonComponent.Entity findMatchBtn;
         public ButtonComponent.Entity cancelMatchBtn;
         public IsVisibleComponent.Entity findMatchBtnVisible;
         public IsVisibleComponent.Entity cancelMatchBtnVisible;
+        public int findMatchEnergyCost;
         public long findMatchTimer;
+        public UIComponentHorseDetail.Entity horseDetail;
+        public UIComponentTraningHorseSelectSumaryList.Entity horseSelectSumaryList;
     }
 
     public ButtonComponent backBtn;
-    public FormattedTextComponent energy;
+    public FormattedTextComponent findMatchEnergyCost;
     public ButtonComponent findMatchBtn;
     public ButtonComponent cancelMatchBtn;
     public IsVisibleComponent findMatchBtnVisible;
     public IsVisibleComponent cancelMatchBtnVisible;
     public FormattedTextComponent findMatchTimer;
+    public UIComponentHorseDetail horseDetail;
+    public UIComponentTraningHorseSelectSumaryList horseSelectSumaryList;
 
     protected override void OnSetEntity()
     {
         backBtn.SetEntity(this.entity.backBtn);
-        energy.SetEntity(this.entity.energy);
+        findMatchEnergyCost.SetEntity(this.entity.findMatchEnergyCost);
         findMatchBtn.SetEntity(this.entity.findMatchBtn);
         cancelMatchBtn.SetEntity(this.entity.cancelMatchBtn);
         findMatchTimer.SetEntity(this.entity.findMatchTimer);
+        horseDetail.SetEntity(this.entity.horseDetail);
+        horseSelectSumaryList.SetEntity(this.entity.horseSelectSumaryList);
     }
 }	
