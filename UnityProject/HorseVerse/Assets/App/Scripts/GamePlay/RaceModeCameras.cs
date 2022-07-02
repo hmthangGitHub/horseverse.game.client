97d67a256370a90119b63020b3a04d6fb71840b8
thang.hoang <bbsort123@gmail.com>
2022-07-02 16:56:37 +0700

    add map settings

---
+++ UnityProject/HorseVerse/Assets/App/Scripts/GamePlay/RaceModeCameras.cs
@@ -0,0 +1,15 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RaceModeCameras : MonoBehaviour
+{
+    public CinemachineClearShot cinemachineClearShot;
+    public void SetHorseGroup(CinemachineTargetGroup targetGroup)
+    {
+        cinemachineClearShot.LookAt = targetGroup.transform;
+        this.GetComponentsInChildren<CameraChangeTriggerer>().ToList().ForEach(x => x.targetGroup = targetGroup);
+    }
+}
