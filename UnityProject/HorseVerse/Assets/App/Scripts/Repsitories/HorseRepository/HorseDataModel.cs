f8c9f5bfd77a159ea219447a55735564d9fd683e
thang.hoang <bbsort123@gmail.com>
2023-04-16 18:41:17 +0700

    remove unused code

--- UnityProject/HorseVerse/Assets/App/Scripts/Repsitories/HorseRepository/HorseDataModel.cs
+++ UnityProject/HorseVerse/Assets/App/Scripts/Repsitories/HorseRepository/HorseDataModel.cs
@@ -27,15 +27,6 @@ public class HorseDataModel
     public float AverageBettingRecord { get; set; }
     public float BestBettingRecord { get; set; }
     public float Rate { get; set; }
-    public float HorizontalSpeed { get; set; }
-    public Vector2 ForwardSpeedRange { get; set; }
-    public float PercentageSpeedBoostWhenSprint { get; set; }
-    public float PercentageSpeedBonusBoostWhenSprintContinuously { get; set; }
-    public Vector2 AccelerationRange { get; set; }
-    public float SprintTime { get; set; }
-    public int SprintChargeNumber { get; set; }
-    public float SprintHealingTime { get; set; }
-    public float SprintBonusTime { get; set; }
     public HorseBasic HorseBasic { get; set; }
     public HorseAttribute HorseAttribute { get; set; }
     public HorseRising HorseRising { get; set; }
